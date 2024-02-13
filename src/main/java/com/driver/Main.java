daf64f333be962c59f11ea6e38abfee0f3c57166
Pradeep <pradeepd6915@gmail.com>
2024-02-13 13:55:24 +0530

    final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,11 +2,11 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args){
-      B obj = new B();
+      A obj = new A();
       String ans1 = obj.meth();
+      System.out.println(ans1);
       B obj1 = new B();
       String ans2 = obj1.meth();
-      System.out.println(ans1);
       System.out.println(ans2);
   }
 }
