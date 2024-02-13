91a5ea84fe2045d11eb6abf388863666efb6e447
Pradeep <pradeepd6915@gmail.com>
2024-02-13 13:37:01 +0530

    third

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args){
-      A obj = new B();
+      B obj = new B();
       String ans1 = obj.meth();
       B obj1 = new B();
       String ans2 = obj1.meth();
