613c1ee5f1308b369e4d939b59e918faeada1884
Pradeep <pradeepd6915@gmail.com>
2024-02-13 13:24:17 +0530

    created classes and objects

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      A obj = new B();
+      String ans1 = obj.meth();
+      B obj1 = new B();
+      String ans2 = obj1.meth();
+  }
 }
